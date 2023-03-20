a9c77ea37d4ca8421c306806b5ec053094fe0b2a
thisisommore <ommore501@gmail.com>
2023-03-20 13:27:28 +0000

    unlock: remove check

--- popup/src/pages/unlock/index.tsx
+++ popup/src/pages/unlock/index.tsx
@@ -1,16 +1,12 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { WalletContext } from "../../contexts/WalletContext";
-import { wallet_exist } from "../../wallet/wallet";
 
 const Unlock = () => {
   const [password, set_password] = useState("");
   const navigate = useNavigate();
   const wallet_context = useContext(WalletContext);
 
-  useEffect(()=>{
-    if(!wallet_exist()) navigate("/create")
-  },[])
   const on_unlock = () => {
     wallet_context.unlock(password);
     navigate("/wallet");
